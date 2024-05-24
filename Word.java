4e17920034f4fa9a8a8aa11adbdfe6034f1dd543
tristaz50 <144478282+tristaz50@users.noreply.github.com>
2024-05-24 12:48:59 -0400

    5/24 Classwork: finish code for word

--- Word.java
+++ Word.java
@@ -1,3 +1,13 @@
 public class Word{
-    
+    public String word;
+
+    public Word(String word) {
+        this.word = word;
+    }
+    public String getWord(){
+        return word;
+    }
+    public boolean isValidLength(){
+        return word.length() == 5;
+    }
 }
